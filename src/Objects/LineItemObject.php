4d78fe2a33263006f0ad3861cc78e2767096ae5d
Rich Porter <rich.porter@carandclassic.com>
2022-08-11 16:08:47 +0100

    Updated LineItem Object with additional properties

--- src/Objects/LineItemObject.php
+++ src/Objects/LineItemObject.php
@@ -9,5 +9,11 @@ class LineItemObject
     public const NAME = 'name';
     public const PRICE = 'price';
     public const QUANTITY = 'quantity';
+    public const SKU = 'hs_sku';
+    public const CURRENCY = 'hs_line_item_currency_code';
     public const FREQUENCY = 'recurringbillingfrequency';
+    public const START_DATE = 'hs_recurring_billing_start_date';
+    public const DESCRIPTION = 'description';
+    public const TERM_MONTHS = 'hs_term_in_months';
+    public const UNIT_DISCOUNT = 'discount';
 }
